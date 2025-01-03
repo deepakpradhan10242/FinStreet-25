ef917b40422ff7f37d207e8716b6b78586469f0f
Deepak Pradhan <130867995+deepakpradhan10242@users.noreply.github.com>
2025-01-03 23:00:14 +0530

    Update authRouter.js

--- server/src/routes/authRouter.js
+++ server/src/routes/authRouter.js
@@ -5,15 +5,11 @@ import logoutUser from '../controllers/logoutUser.js';
 import isAuthenticated from '../middleware/isAuthenticated.js';
 import userAuth from '../middleware/userAuth.js';
 
-// const verifyToken = require('../middleware/verifyToken');
-// const dashboard = require('../controllers/dashboard');
-
 
 const authRouter = express.Router();
 
-//create user api
 authRouter.post('/register',registerUser);
-//check user login
+
 authRouter.post('/login',loginUser);
 
 authRouter.post('/logout',logoutUser);
